28e0a90440b17bec637b28c0912a40f687cc2d0b
Dakaiitt <dakaiitt@gmail.com>
2021-09-03 20:46:58 +0900

    default tab fixed

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -197,7 +197,7 @@ const Divider = styled.div`
 
 export default function SimpleTabs({ result, collection }) {
   const classes = useStyles();
-  const [value, setValue] = React.useState(2);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
